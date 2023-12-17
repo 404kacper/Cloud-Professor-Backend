aff14cca45f96ff30df96228c45b6425f0f76c58
404kacper <kacper.sionkowski@gmail.com>
2023-12-17 21:47:42 +0100

    Changed maximum allowed payload to allow larger files from frontend

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "100mb",
+      jsonLimit: "100mb",
+      textLimit: "100mb",
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024, // multipart data - allowed size
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
