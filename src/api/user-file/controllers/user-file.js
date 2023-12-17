b132ded384a2fcd46cf8b3c48068930b9d14d645
404kacper <kacper.sionkowski@gmail.com>
2023-12-17 22:07:43 +0100

    Accounted for larger files in DELETE action

--- src/api/user-file/controllers/user-file.js
+++ src/api/user-file/controllers/user-file.js
@@ -1,9 +1,29 @@
-'use strict';
+"use strict";
 
 /**
  * user-file controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::user-file.user-file');
+module.exports = createCoreController(
+  "api::user-file.user-file",
+  ({ strapi }) => ({
+    // wrap the default controller actions
+    // remove attributes from the response
+    // since contents field can get large now & frontend only uses id to sort attributes are uncessary
+    async delete(ctx) {
+      // Call the default core action
+      const response = await super.delete(ctx);
+
+      // Modify the response to remove the 'contents' field
+      // the custom logic part
+      if (response.data) {
+        delete response.data.attributes;
+      }
+
+      // Return the modified response
+      return response;
+    },
+  })
+);
