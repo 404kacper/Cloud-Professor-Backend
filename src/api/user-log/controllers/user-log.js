57b075bdef300a3fad97d4b08894a82bca5e0118
404kacper <kacper.sionkowski@gmail.com>
2023-12-19 18:40:34 +0100

    Completed backend for logs implementation

--- src/api/user-log/controllers/user-log.js
+++ src/api/user-log/controllers/user-log.js
@@ -1,9 +1,42 @@
-'use strict';
+"use strict";
 
 /**
  * user-log controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::user-log.user-log');
+module.exports = createCoreController(
+  "api::user-log.user-log",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // Get the logged-in user's ID
+      const userId = ctx.state.user?.id;
+
+      // Check if a user is logged in
+      if (!userId) {
+        return ctx.badRequest("No authenticated user found");
+      }
+
+      const response = await strapi.entityService.findMany(
+        "api::user-log.user-log",
+        {
+          filters: {
+            user: userId,
+          },
+          limit: 25,
+          fields: ["id", "createdAt", "associatedFile"],
+          // set hard limit of 25 so that the query doesn't get too big
+          populate: {
+            user: {
+              fields: ["id", "publicKey"],
+            },
+          },
+        }
+      );
+
+      // Return the response
+      return response;
+    },
+  })
+);
